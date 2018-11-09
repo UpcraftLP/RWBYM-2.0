1d68410a603d3f6942eb45051345fd6e7acf6df1
UpcraftLP <j-schirra@gmx.de>
2018-11-09 16:19:56 +0100

    fix capabilities not being attached properly

--- src/main/java/be/bluexin/rwbym/capabilities/CapabilityHandler.java
+++ src/main/java/be/bluexin/rwbym/capabilities/CapabilityHandler.java
@@ -2,17 +2,20 @@ package be.bluexin.rwbym.capabilities;
 
 import be.bluexin.rwbym.RWBYModels;
 import be.bluexin.rwbym.capabilities.ruby.RubyProvider;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
+@Mod.EventBusSubscriber(modid = RWBYModels.MODID)
 public class CapabilityHandler {
 	
 	public static final ResourceLocation RUBY_CAP = new ResourceLocation(RWBYModels.MODID, "ruby");
 	
 	@SubscribeEvent
-	public void attachCapability(AttachCapabilitiesEvent event) {
-		event.addCapability(RUBY_CAP, new RubyProvider());
+	public static void attachCapability(AttachCapabilitiesEvent<Entity> event) {
+		if(event.getObject() instanceof EntityPlayer) event.addCapability(RUBY_CAP, new RubyProvider());
 	}
 }
